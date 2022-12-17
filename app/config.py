c8863f9925e0e3d6c1c42e55b5e71fb1d19a32ee
logicguy1 <logicguy.mailandcontact@gmail.com>
2022-12-17 14:24:02 +0100

    Update config setup

--- app/config.py
+++ app/config.py
@@ -2,18 +2,16 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    UPLOADED_IMAGES_DEST = os.environ.get('UPLOADED_IMAGES_DEST') or 'app/assets/uploads'
+    SECRET_KEY = "you-will-never-guess"
+    UPLOADED_IMAGES_DEST = "app/assets/uploads"
     TIME_STR = "%Y-%m-%d %X"
     WORDLIST = "wordlistDK.txt"
     DEBUG = False
     VERSION = "0.1"
-    NGINX_LOGS = os.environ.get("NGINX_LOGS") or '_tests/access.log'
-
-    # Wheater or not to use multi threading when handeling images
-    QUICK_MODE = False 
+    NGINX_LOGS = "_tests/access.log"
+    # NGINX_LOGS = "/var/log/nginx/access.log"
 
     # SQLAlchemy database config, track modifications is set for compadabilaty with SQLite
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    # SQLALCHEMY_DATABASE_URI = "mysql://user:pwsswd@localhost:3306/reciptdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
